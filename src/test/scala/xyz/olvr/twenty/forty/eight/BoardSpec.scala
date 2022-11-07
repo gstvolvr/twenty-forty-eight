d6ecd2b5279ebb757c40dc07284cf9ec478d0107
Gustavo A. Oliver <gstvolvr@users.noreply.github.com>
2022-11-06 19:34:21 -0500

    update board spec

--- src/test/scala/xyz/olvr/twenty/forty/eight/BoardSpec.scala
+++ src/test/scala/xyz/olvr/twenty/forty/eight/BoardSpec.scala
@@ -6,20 +6,21 @@ class BoardSpec extends FunSpec {
   import Board._
 
   val emptyRow: Row = Vector.fill(4)(0)
-  val board: Board = Vector(Vector(0, 0, 2, 0), emptyRow, emptyRow, Vector(0, 2, 0, 0))
+  val board: Board = Vector(Vector(0, 2, 2, 0), emptyRow, emptyRow, Vector(0, 2, 0, 0))
 
   describe("test the universe") {
     it("should move up") {
-      assert(up(board) === Vector(Vector(0, 2, 2, 0)) ++ Vector.fill(3)(emptyRow))
+      assert(up(board) === Vector(Vector(0, 3, 2, 0)) ++ Vector.fill(3)(emptyRow))
+
     }
     it("should move down") {
-      assert(down(board) === Vector.fill(3)(emptyRow) :+ Vector(0, 2, 2, 0))
+      assert(down(board) === Vector.fill(3)(emptyRow) :+ Vector(0, 3, 2, 0))
     }
     it("should move right") {
-      assert(right(board) === Vector(Vector(0, 0, 0, 2)) ++ Vector.fill(2)(emptyRow) :+ Vector(0, 0, 0, 2))
+      assert(right(board) === Vector(Vector(0, 0, 0, 3)) ++ Vector.fill(2)(emptyRow) :+ Vector(0, 0, 0, 2))
     }
     it("should move left") {
-      assert(left(board) === Vector(Vector(2, 0, 0, 0)) ++ Vector.fill(2)(emptyRow) :+ Vector(2, 0, 0, 0))
+      assert(left(board) === Vector(Vector(3, 0, 0, 0)) ++ Vector.fill(2)(emptyRow) :+ Vector(2, 0, 0, 0))
     }
     it("should add a random tile") {
       assert(randomTile(board) !== board)
